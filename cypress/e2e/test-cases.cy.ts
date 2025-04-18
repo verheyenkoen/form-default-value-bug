028486d1a37548aa182ca54e6fd3d4240b9d5866
Koen Verheyen <verheyen.koen@gmail.com>
2025-04-18 10:07:19 +0200

    Added cypress test

---
+++ cypress/e2e/test-cases.cy.ts
@@ -0,0 +1,35 @@
+const TEST_CASES = {
+  "<Form /> + <Link />": "form-plus-link-component",
+};
+
+Object.entries(TEST_CASES).forEach(([name, slug]) => {
+  describe(`The ${name} test case`, () => {
+    it("should work", () => {
+      cy.visit(`/${slug}`);
+
+      cy.get("#renderedSearchQuery").should("have.text", "(empty)");
+      cy.get("#searchField").should("have.value", "");
+
+      cy.contains("a", "Change search query").click();
+      cy.get("#renderedSearchQuery").should("have.text", "changed");
+      cy.get("#searchField").should("have.value", "changed");
+
+      cy.contains("a", "Empty search query").click();
+      cy.get("#renderedSearchQuery").should("have.text", "(empty)");
+      cy.get("#searchField").should("have.value", "");
+
+      cy.get("#searchField").type("something");
+      cy.contains("button", "Submit").click();
+      cy.get("#renderedSearchQuery").should("have.text", "something");
+      cy.get("#searchField").should("have.value", "something");
+
+      cy.contains("a", "Change search query").click();
+      cy.get("#renderedSearchQuery").should("have.text", "changed");
+      cy.get("#searchField").should("have.value", "changed");
+
+      cy.contains("a", "Empty search query").click();
+      cy.get("#renderedSearchQuery").should("have.text", "(empty)");
+      cy.get("#searchField").should("have.value", "");
+    });
+  });
+});
